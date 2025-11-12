70c480b1be4cc03c3e6ee66cac5c613c51610bc1
Lite <maksarandaev@gmail.com>
2025-11-12 11:38:40 +0700

    Добавил счетчик посещений

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,11 +20,16 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
         'num_books_with_word': num_books_with_word,
         'num_genres_with_word': num_genres_with_word,
         'search_word': 'Детектив',
